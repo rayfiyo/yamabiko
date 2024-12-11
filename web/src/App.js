d2d891f3b3fe376bacd87701a02e2408def35091
rayfiyo <rayfiyorex@gmail.com>
2024-12-11 23:04:49 +0900

    fix: 🐛 App.js と Home.js に分割することで，真っ白なバグを修正（正しくルーティング
    ✅ Closes: #2

--- web/src/App.js
+++ web/src/App.js
@@ -1,53 +1,17 @@
 import React from "react";
-import { Container, Form, Button } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 import "./App.css";
-// import SocialTimelineSearchResult from "./SocialTimelineSearchResult";
-// <SocialTimelineSearchResult />
-
-import header from "./images/yamabiko-header.png";
-import megaphone from "./images/icons/megaphone.svg";
 
 const App = () => {
-  const navigate = useNavigate();
-
-  function shout() {
-    navigate("/timeline");
-  }
-
   return (
-    <Container fluid>
-      <img
-        src={header}
-        alt="the yamabiko's header"
-        className="img-fluid d-block mx-auto mt-5 w-50"
-      />
-      <Form>
-        <div className="d-flex gap-1 mt-3">
-          <Form.Group
-            controlId="shout"
-            className="flex-grow-1 align-self-center"
-          >
-            <Form.Control placeholder="どんな話題がある～？" />
-          </Form.Group>
-
-          <Button type="submit" onClick={shout}>
-            <img
-              src={megaphone}
-              alt="shout (generally means submit) icon"
-              className="img-fluid"
-              style={{ width: "30px" }}
-            />
-          </Button>
-        </div>
-
-        <Form.Group controlId="demoMode" className="d-flex flex-row-reverse">
-          <Form.Check type="checkbox" label="Demo mode results" />
-        </Form.Group>
-      </Form>
-      ( // SocialTimelineSearchResult )
-    </Container>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
 export default App;
+// <Route path="/timeline" element={<TimelinePage />} />
