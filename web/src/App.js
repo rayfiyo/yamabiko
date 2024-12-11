8eb5d180eb74607f3df5fe49af110c8c9cbcd998
rayfiyo <rayfiyorex@gmail.com>
2024-12-11 23:41:31 +0900

    feat: 🍻 timeline ひとまずページを作った

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import Timeline from "./Timeline";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/timeline" element={<Timeline />} />
       </Routes>
     </Router>
   );
 };
 
 export default App;
-// <Route path="/timeline" element={<TimelinePage />} />
