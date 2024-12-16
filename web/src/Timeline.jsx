cf5366db5bf549b76d1ee640450154ceeef59d43
rayfiyo <rayfiyorex@gmail.com>
2024-12-16 10:21:12 +0900

    refactor: 💡 コメントアウト削除

--- web/src/Timeline.jsx
+++ web/src/Timeline.jsx
@@ -6,10 +6,6 @@ import ListGroup from "react-bootstrap/ListGroup";
 import down from "./images/icons/down.svg";
 import up from "./images/icons/up.svg";
 
-// import React, { useState } from "react";
-// import { faSearch } from "@fortawesome/free-solid-svg-icons";
-// import { useHistory } from "react-router-dom";
-
 const Timeline = () => {
   const userName = "hoge hoge 男";
   const content =
@@ -72,43 +68,3 @@ const Timeline = () => {
 };
 
 export default Timeline;
-
-/*
-  const [searchQuery, setSearchQuery] = useState("");
-  // const [posts, setPosts] = useState([
-  // 投稿データ;
-  // ]);
-  // const history = useHistory();
-
-  const handleSearch = (e) => {
-    setSearchQuery(e.target.value);
-    // 検索機能の実装
-  };
-
-  const handleSubmit = () => {
-    // 検索クエリをURLのパラメータとして送る
-    // history.push(`/search?q=${encodeURIComponent(searchQuery)}`);
-  };
-
-
-
-      <div className="container my-5">
-        <div className="row mb-4">
-          <div className="col">
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Search posts..."
-                value={searchQuery}
-                onChange={handleSearch}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="row">{
-        // 投稿一覧の表示
-        }</div>
-      </div>
-*/
